c920397aff4728ed2dc5ef7736ee71f8de60148b
Yash <yashprajapati.9769@gmail.com>
2026-02-26 20:32:38 +0530

    26 feb final changes

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,17 @@ const noteRoutes = require("./src/routes/notes.routes");
 const dotenv = require("dotenv");
 const connectDB = require("./src/config/db");
 const rateLimiter = require("./src/middleware/rateLimiter");
-const app = express();
 const cors = require("cors");
 const path = require("path");
 
 dotenv.config();
-const __dirname = path.resolve();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
-const PORT = process.env.PORT || 5001;
 app.use(rateLimiter);
+
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
@@ -20,19 +21,14 @@ if (process.env.NODE_ENV !== "production") {
     }),
   );
 }
-// app.use((req, res, next) => {
-//   console.log(`method:${req.method} and url was:${req.url}`);
-//   next();
-// });
 
 app.use("/api/notes", noteRoutes);
-app.use(express.static(path.join(__dirname, "../frontend")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
