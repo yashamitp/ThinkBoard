c0a5cdf2314e82bd3a7b4f06c30f537b8fc9f5d5
Yash <yashprajapati.9769@gmail.com>
2026-02-26 18:26:31 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,29 @@ const dotenv = require("dotenv");
 const connectDB = require("./src/config/db");
 const rateLimiter = require("./src/middleware/rateLimiter");
 const app = express();
-const PORT = process.env.PORT || 5001;
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
+const __dirname = path.resolve();
 
 app.use(express.json());
+const PORT = process.env.PORT || 5001;
 app.use(rateLimiter);
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 // app.use((req, res, next) => {
 //   console.log(`method:${req.method} and url was:${req.url}`);
 //   next();
 // });
 
 app.use("/api/notes", noteRoutes);
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
