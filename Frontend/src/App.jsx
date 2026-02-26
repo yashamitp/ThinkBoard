d8963ea20f73bef5c2551271d0ed2c1ffde6dfad
Yash <yashprajapati.9769@gmail.com>
2026-02-26 17:21:02 +0530

    new changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,19 @@
-import React from "react";
-import { Route, Router, Routes } from "react-router";
+import { Route, Routes } from "react-router";
+
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
-import toast from "react-hot-toast";
-import RateLimitedUI from "./components/RateLimitUi";
 
-function App() {
+const App = () => {
   return (
-    <div>
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/createpage" element={<CreatePage />} />
-        <Route path="notedetail" element={<NoteDetailPage />} />
-        <Route path="ratelimitui" element={<RateLimitedUI />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
   );
-}
-
+};
 export default App;
